0c6b98756741a61fbc384f7666e9ead2b0ea92c4
krister <krister@sent.at>
2019-07-14 17:16:55 +0300

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+interface TransformOptions {
+  prop: string;
+  value: string;
+  win?: {
+    width: number;
+    height: number;
+  };
+  parent?: {
+    width: number;
+    height: number;
+  };
+  font?: {
+    size: number;
+  };
+}
+
+export function transform(options: TransformOptions): number;
